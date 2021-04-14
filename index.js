0f3f49e3628c75ef3a81f0ce74dae824acbd7de5
unknown <harshar715@gmail.com>
2021-04-14 15:03:19 +0530

    changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const graphqlSchemas = require('./graphql/schemas/index');
 const graphqlResolvers = require('./graphql/resolvers/index');
@@ -10,21 +11,23 @@ const isAuth = require('./middleware/is-auth');
 
 const app = express();
 
+app.use(cors());
+
 app.use(bodyparser.json());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public', { maxAge: 31557600 }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-    res.setHeader('Access-control-Allow-Headers', 'Content-Type, Authorization');
-    if(req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+//     res.setHeader('Access-control-Allow-Headers', 'Content-Type, Authorization');
+//     if(req.method === 'OPTIONS') {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// })
 
 app.use(isAuth);
 
@@ -39,4 +42,4 @@ mongoose.connect(`mongodb+srv://${config.env.MONGO_USER}:${config.env.MONGO_PASS
 mongoose.Promise = global.Promise;
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`app listening at ${port}`))
+app.listen(PORT, () => console.log(`app listening at ${PORT}`))
