2461c9daa7db09403ecdb23f1ab4041a770971c5
unknown <harshar715@gmail.com>
2021-04-14 13:23:12 +0530

    Lastest changes

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const mongoose = require('mongoose');
+
+const graphqlSchemas = require('./graphql/schemas/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
+const config = require('./config.json');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
+
+app.use(bodyparser.json());
+
+app.use(express.static('public', { maxAge: 31557600 }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
+
+app.use(isAuth);
+
+app.use('/graphql', graphqlHttp({
+    schema: graphqlSchemas,
+    rootValue: graphqlResolvers,
+    graphiql: true
+}));
+
+mongoose.connect(`mongodb+srv://${config.env.MONGO_USER}:${config.env.MONGO_PASS}@mydatabase.imncy.mongodb.net/${config.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { console.log('connected to db') }).catch(err => console.log(err));
+mongoose.Promise = global.Promise;
+
+const port = 3000 || process.env;
+app.listen(port, () => console.log(`app listening at ${port}`))
