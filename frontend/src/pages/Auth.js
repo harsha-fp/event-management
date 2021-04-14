47b2611d6a78997953c4ba22e34ea57eef174a68
unknown <harshar715@gmail.com>
2021-04-14 19:30:32 +0530

    Added Error, Success, Loading messages

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -6,7 +6,10 @@ const config = require('../assets/config.json');
 
 class AuthPage extends Component {
     state = {
-        isLogin: true
+        isLogin: true,
+        showSuccess: false,
+        showError: false,
+        isLoading: false
     }
 
     static contextType = authContext;
@@ -24,6 +27,7 @@ class AuthPage extends Component {
     }
 
     submitHandler = (event) => {
+        this.setState({ isLoading: true });
         event.preventDefault();
         const email = this.emailElement.current.value;
         const password = this.passwordElement.current.value;
@@ -71,38 +75,65 @@ class AuthPage extends Component {
                 return res.json();
             })
             .then(data => {
-                if (data.data.login.token) {
-                    this.context.login(
-                        data.data.login.token,
-                        data.data.login.userId,
-                        data.data.login.tokenExpiration
-                    );
+                if (data.hasOwnProperty('errors')) {
+
+                    this.setState({ showError: true, isLoading: false });
+                    setTimeout(() => {
+                        this.setState({ showError: false });
+                    }, 3000);
+                    return;
+
+                } else {
+                    if (data.data.hasOwnProperty('login')) {
+                        this.context.login(
+                            data.data.login.token,
+                            data.data.login.userId,
+                            data.data.login.tokenExpiration
+                        );
+
+                    } else {
+                        this.setState({ showSuccess: true, isLoading: false });
+                        setTimeout(() => {
+                            this.setState({ showSuccess: false });
+                        }, 3000);
+                    }
                 }
 
             })
             .catch(err => {
                 console.log(err);
+                this.setState({ showError: true, isLoading: false });
+                setTimeout(() => {
+                    this.setState({ showError: false });
+                }, 3000);
             })
     };
 
     render() {
         return (
+            <React.Fragment>
+                <form className="auth-form" onSubmit={this.submitHandler}>
+                    <h2>{this.state.isLogin ? 'Login' : 'Register'}</h2>
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" ref={this.emailElement} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" ref={this.passwordElement} />
+                    </div>
+                    <div className="form-actions">
+                        <button type="submit">Submit</button>
+                        <button type="button" onClick={this.switchModeHandler}>Switch to {this.state.isLogin ? 'Signup' : 'Login'}</button>
+                    </div>
+                </form>
 
-            <form className="auth-form" onSubmit={this.submitHandler}>
-                <h2>{this.state.isLogin ? 'Login' : 'Register'}</h2>
-                <div className="form-control">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" ref={this.emailElement} />
+                <div className="message">
+                    {this.state.isLoading && (<h3>Sending request...!! Please wait.</h3>)}
+                    {this.state.showSuccess && (<h3>Registration Successful!! Please Login.</h3>)}
+                    {this.state.showError && (<h3>Error Occured!! Try Again.</h3>)}
                 </div>
-                <div className="form-control">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" ref={this.passwordElement} />
-                </div>
-                <div className="form-actions">
-                    <button type="submit">Submit</button>
-                    <button type="button" onClick={this.switchModeHandler}>Switch to {this.state.isLogin ? 'Signup' : 'Login'}</button>
-                </div>
-            </form>
+            </React.Fragment>
         )
     }
 }
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const isAuth = require('./middleware/is-auth');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(bodyparser.json());
 
@@ -19,15 +19,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public', { maxAge: 31557600 }));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-//     res.setHeader('Access-control-Allow-Headers', 'Content-Type, Authorization');
-//     if(req.method === 'OPTIONS') {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// })
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
 
 app.use(isAuth);
 
